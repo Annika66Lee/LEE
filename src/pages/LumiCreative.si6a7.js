1916a7f39c39b7e5bd244286429312368f93e20e
Zhenyao Liu <liu59@seas.upenn.edu>
2026-06-18 16:56:37 -0700

    Rename LumiCreative page heading

--- src/pages/LumiCreative.si6a7.js
+++ src/pages/LumiCreative.si6a7.js
@@ -5,4 +5,15 @@ import { applyLumiTheme } from 'public/lumiTheme.js';
 
 $w.onReady(function () {
     applyLumiTheme($w);
+
+    $w('Text').forEach((textElement) => {
+        const headingPattern = /lumi\s*(?:creative|vreative)\s*incubator/i;
+
+        if (headingPattern.test(textElement.text)) {
+            textElement.text = textElement.text.replace(
+                new RegExp(headingPattern.source, 'gi'),
+                'LumiCreative12345'
+            );
+        }
+    });
 });
